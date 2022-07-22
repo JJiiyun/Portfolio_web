93973aded73d7adba6a6f112b7dd7e1f81bd06c3
JJiiyun <0607030@naver.com>
2022-07-22 12:44:37 +0900

    JS: contact me button

--- main.js
+++ main.js
@@ -12,27 +12,37 @@ document.addEventListener('scroll', ()=> {
     }
 })
 
-// click to page code from Ellie
-// const navbarMenu = document.querySelector('.navbar__menu');
-// navbarMenu.addEventListener('click', (event) => {
-//     const target = event.target;
-//     const link = target.dataset.link;
-    
-//     if(link==null) {
-//         return;
-//     }
-    
-//     const scrollTo = document.querySelector(link);
-//     scrollTo.scrollIntoView({behavior: "smooth"});
-// })
 
 //click to page code from googling + for문. 효율적인 코드인지는 잘 모르겠으나 navbarHeight을 빼고 싶어서
-const navbarMenu = document.querySelectorAll(".navbar__menu__item");
-const sections = document.querySelectorAll("section");
+// const navbarMenu = document.querySelectorAll(".navbar__menu__item");
+// const sections = document.querySelectorAll("section");
 
-for(let i=0; i<navbarMenu.length; i++) {
-    const top = sections[i].offsetTop - navbarHeight;
+// for(let i=0; i<navbarMenu.length; i++) {
+    //     const top = sections[i].offsetTop - navbarHeight;
 
-    navbarMenu[i].onclick = () => {
-    window.scroll({top: top, behavior: 'smooth'})
-}}
+    //     navbarMenu[i].onclick = () => {
+//     window.scroll({top: top, behavior: 'smooth'})
+// }}
+
+
+
+// click to page code from Ellie
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if(link == null) {return;}
+    scrollIntoView(link);
+});
+
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
